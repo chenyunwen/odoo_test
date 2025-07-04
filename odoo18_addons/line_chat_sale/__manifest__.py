fed9bda334436d59c02fc631e01d4151424d0b32
chenyunwen <sssh10110@gmail.com>
2025-07-04 15:44:34 +0800

    fix(line_chat_sale): update 'depends' to ensure required modules are imported automatically

--- odoo18_addons/line_chat_sale/__manifest__.py
+++ odoo18_addons/line_chat_sale/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'category': 'Sales/Line',
     'summary': 'Line Chat 之擴充模組，可在銷售頁面將訂單資訊透過 LINE 傳送給已具 LINE 聯繫方式的客戶。',
-    'depends': ['base', 'mail', 'sale'],
+    'depends': ['base', 'mail', 'line_chat', 'sale_management', 'sale'],
     'data': [
       'views/sale_order_views.xml',
     ],
