98e79dd2a754608375fbbee071f9608dfbb8a2f0
chenyunwen <sssh10110@gmail.com>
2025-07-04 15:29:50 +0800

    fix(line_chat): remove invalid post_init_hook to allow module import

--- odoo18_addons/line_chat/__manifest__.py
+++ odoo18_addons/line_chat/__manifest__.py
@@ -3,17 +3,15 @@
     'version': '1.0',
     'category': 'Sales/Line',
     'summary': '將 Odoo 中的客服訊息與 LINE 官方帳號串接，並支援圖片與音訊回覆。',
-    'depends': ['base', 'mail', ], # ,'sale'
+    'depends': ['base', 'mail', ],
     'data': [
       'security/ir.model.access.csv',
       'views/line_chat_views.xml',
       'views/line_chat_status_views.xml',
       'views/line_image_set_views.xml',
-      # 'views/sale_order_views.xml',
       'views/res_config_settings_views.xml',
     ],
     'images': ['static/description/icon.png'],
     'installable': True,
     'application': True,
-    'post_init_hook': 'create_default_channel',
 }
