76db682448d05b0756870e93d18294293d93541b
chenyunwen <sssh10110@gmail.com>
2025-07-24 18:22:21 +0800

    feat: auto check-out when upload image

--- odoo18_addons/hr_attendance_image/models/hr_attendance.py
+++ odoo18_addons/hr_attendance_image/models/hr_attendance.py
@@ -1,8 +1,23 @@
 # models/hr_attendance.py
-from odoo import _, models, fields
+from odoo import _, api, models, fields
 
 class HrAttendance(models.Model):
     _inherit = 'hr.attendance'
 
-    photo = fields.Binary(string=_('Check-in Photo'))
+    check_in = fields.Datetime(string="Check In", default=fields.Datetime.now, required=True, tracking=True,readonly=True)
+    check_out = fields.Datetime(string="Check Out", tracking=True, readonly=True)
+    photo = fields.Binary(string=_('Check-in Photo'),required=True)
     photo_filename = fields.Char(string=_('Photo Filename'))
+
+    def create(self, vals):
+        if vals.get('photo'):
+            vals['check_in'] = fields.Datetime.now()
+            vals['check_out'] = fields.Datetime.now()
+        return super().create(vals)
+    
+    def write(self, vals):
+        if vals.get('photo'):
+            vals['check_in'] = fields.Datetime.now()
+            vals['check_out'] = fields.Datetime.now()
+        return super().write(vals)
+        
